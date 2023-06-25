05e92214a88172058a8a2cfa98603757b62df929
Yoonbae Cho <y@xcv.kr>
2023-06-25 08:00:05 +0400

    Update

---
+++ src/yQuant.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yQuant.Domain.Enums;
+
+namespace yQuant.Domain.Entities;
+
+public class Order {
+    public OrderStatus Status { get; }
+    public string Symbol { get; }
+    public long Quantity { get; }
+}
